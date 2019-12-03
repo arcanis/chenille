d974d0701df1c8c4ab818443c888938fcfadffc0
Maël Nison <nison.mael@gmail.com>
2019-12-03 18:04:13 +0100

    Adds author

--- sources/git/sendToMergeQueue.js
+++ sources/git/sendToMergeQueue.js
@@ -1,6 +1,8 @@
 exports.sendToMergeQueue = async (git, {number, title}, hash) => {
+  const author = await git(`log`, `-1`, `--pretty=format:'%an <%ae>'`, hash);
+
   await git(`checkout`, `merge-queue`);
   await git(`merge`, `--squash`, hash);
   await git.prefixSquashMessage(`[#${number}] ${title}\n\n`);
-  await git(`commit`, `--no-edit`);
+  await git(`commit`, `--no-edit`, `--author`, author);
 };
