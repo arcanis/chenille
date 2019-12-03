27a33f9e2c3caaff680cf4b15d35c2cd3d946673
Maël Nison <nison.mael@gmail.com>
2019-12-03 15:23:20 +0100

    Fixes parameters

--- sources/github/pushToOrigin.js
+++ sources/github/pushToOrigin.js
@@ -1,3 +1,3 @@
 exports.pushToOrigin = async (git, ...args) => {
-  await git(`push`, `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com:${process.env.GITHUB_REPOSITORY}.git`, ...args);
+  await git(`push`, `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`, ...args);
 };
