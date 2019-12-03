b849a9db8225e7fabdbf1cb84ccecc0d9631bcb7
Maël Nison <nison.mael@gmail.com>
2019-12-03 16:32:53 +0100

    Fixes argument order

--- sources/github/pushToOrigin.js
+++ sources/github/pushToOrigin.js
@@ -1,3 +1,3 @@
 exports.pushToOrigin = async (git, ...args) => {
-  await git(`push`, `-c`, `http.extraheader=Authorization: basic ${process.env.GITHUB_TOKEN}`, `origin`, ...args);
+  await git(`-c`, `http.extraheader=Authorization: basic ${process.env.GITHUB_TOKEN}`, `push`, `origin`, ...args);
 };
