47a21a35275311bb315820947e37ebfee45b1356
Maël Nison <nison.mael@gmail.com>
2019-12-03 14:31:59 +0100

    Fixes context passing

--- sources/github/actionEntry.js
+++ sources/github/actionEntry.js
@@ -7,26 +7,32 @@ const {getEventFile} = require(`./getEventFile`);
 
 const eventFile = getEventFile();
 
+const context = {
+  stdin: process.stdin,
+  stdout: process.stdout,
+  stderr: process.stderr,
+};
+
 switch (process.env.GITHUB_EVENT_NAME) {
   case `issue_comment`: {
     if (eventFile.action === `created` && eventFile.issue.pull_request && eventFile.issue.state === `open`) {
-      cli.runExit([`dispatch`, `comment`, `to`, eventFile.issue.number, `--cwd=${process.env.GITHUB_WORKSPACE}`, `--title=${eventFile.issue.title}`, `--body=${eventFile.issue.body}`]);
+      cli.runExit([`dispatch`, `comment`, `to`, eventFile.issue.number, `--cwd=${process.env.GITHUB_WORKSPACE}`, `--title=${eventFile.issue.title}`, `--body=${eventFile.issue.body}`], context);
     }
   } break;
 
   case `pull_request`: {
     if (eventFile.action === `synchronize`) {
-      cli.runExit([`cancel`, eventFile.number, `--cwd=${process.env.GITHUB_WORKSPACE}`, `--title=${eventFile.pull_request.title}`]);
+      cli.runExit([`cancel`, eventFile.number, `--cwd=${process.env.GITHUB_WORKSPACE}`, `--title=${eventFile.pull_request.title}`], context);
     }
   } break;
 
   case `push`: {
     if (eventFile.ref === `refs/heads/master`) {
-      cli.runExit([`resync`, `--cwd=${process.env.GITHUB_WORKSPACE}`]);
+      cli.runExit([`resync`, `--cwd=${process.env.GITHUB_WORKSPACE}`], context);
     }
   } break;
 
   case `status`: {
-    cli.runExit([`dispatch`, `status`, `--driver=${require.resolve(`./fetchCommitStatus`)}`]);
+    cli.runExit([`dispatch`, `status`, `--driver=${require.resolve(`./fetchCommitStatus`)}`], context);
   } break;
 }
