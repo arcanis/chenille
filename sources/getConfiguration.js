9745420564d321208aa4ed99e777c1fae05e0437
Maël Nison <nison.mael@gmail.com>
2019-12-18 14:43:18 +0100

    Dont use ESM

--- sources/getConfiguration.js
+++ sources/getConfiguration.js
@@ -1,6 +1,6 @@
 const yaml = require(`js-yaml`);
 
-export const BASE_CONFIGURATION = {
+exports.BASE_CONFIGURATION = {
   branches: {
     master: process.env.INPUT_MASTER || `master`,
     mergeQueue: process.env.INPUT_MERGEQUEUE || `merge-queue`,
@@ -10,13 +10,13 @@ export const BASE_CONFIGURATION = {
 exports.getConfiguration = async git => {
   let configFile;
   try {
-    configFile = await git(`show`, `${BASE_CONFIGURATION.branches.master}:chenille.yml`);
+    configFile = await git(`show`, `${exports.BASE_CONFIGURATION.branches.master}:chenille.yml`);
   } catch {
     return {};
   }
 
   return {
     ...yaml.load(configFile),
-    ...BASE_CONFIGURATION,
+    ...exports.BASE_CONFIGURATION,
   };
 };
