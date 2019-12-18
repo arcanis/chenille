aaae53de26d11255772d2c0c5b4db81c2038bc07
Maël Nison <nison.mael@gmail.com>
2019-12-18 14:04:11 +0100

    Stops reading the user information from the config

--- sources/git/sendToMergeQueue.js
+++ sources/git/sendToMergeQueue.js
@@ -1,11 +1,17 @@
 exports.sendToMergeQueue = async (git, pr, hash) => {
   try {
-    const author = await git(`log`, `-1`, `--pretty=format:'%an <%ae>'`, hash);
+    const authorName = await git(`log`, `-1`, `--pretty=format:'%an'`, hash);
+    const authorEmail = await git(`log`, `-1`, `--pretty=format:'%ae'`, hash);
+
+    const author = [
+      `-c`, `user.name=${authorName}`,
+      `-c`, `user.email=${authorEmail}`,
+    ];
 
     await git(`checkout`, `merge-queue`);
     await git(`merge`, `--squash`, hash);
     await git.prefixSquashMessage(`[#${pr.number}] ${pr.title}\n\nCloses: #${pr.number}\n\n`);
-    await git(`commit`, `--no-edit`, `--author`, author);
+    await git(...author, `commit`, `--no-edit`, `--author`, `${authorName} <${authorEmail}>`);
 
     return [];
   } catch (error) {
