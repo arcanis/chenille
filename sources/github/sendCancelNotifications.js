0a0f904dab3b94fe24d1c841089e14e6363842aa
Maël Nison <nison.mael@gmail.com>
2019-12-18 17:18:53 +0100

    Fixes url

--- sources/github/sendCancelNotifications.js
+++ sources/github/sendCancelNotifications.js
@@ -4,8 +4,7 @@ exports.sendCancelNotifications = async (prs) => {
   const [owner, name] = process.env.GITHUB_REPOSITORY.split(/\//);
 
   for (const pr of prs) {
-    console.log(`https://api.github.com/v3/repos/${owner}/${name}/issues/${pr.number}/comments`);
-    await got.post(`https://api.github.com/v3/repos/${owner}/${name}/issues/${pr.number}/comments`, {
+    await got.post(`https://api.github.com/repos/${owner}/${name}/issues/${pr.number}/comments`, {
       json: true,
       headers: {
         Authorization: `token ${process.env.GITHUB_TOKEN}`,
