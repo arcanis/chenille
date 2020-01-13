bd7bebeed35f5767d61a207f131d84b090ad10ca
Maël Nison <nison.mael@gmail.com>
2020-01-13 19:52:26 +0100

    Fixes missing symbol

--- sources/setupDevEnv.js
+++ sources/setupDevEnv.js
@@ -4,6 +4,7 @@
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
  * Copyright (c) 2020-Present Datadog, Inc.
  */
+const path = require(`path`);
 const root = path.dirname(__dirname);
 
 require(`@babel/register`)({
