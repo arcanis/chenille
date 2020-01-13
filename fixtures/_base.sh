60bf64bdcffac3b7405e3d29fcde0e8a5995381b
Maël Nison <nison.mael@gmail.com>
2020-01-13 14:01:49 +0100

    Uses environment

--- fixtures/_base.sh
+++ fixtures/_base.sh
@@ -1,12 +1,15 @@
 MASTER=master
 MERGE_QUEUE=merge-queue
 
-AUTHOR_CONFIG="-c user.email=postmaster@example.org -c user.name=Example"
-AUTHOR_OPTION=--author="Example <postmaster@example.org>"
+export GIT_AUTHOR_NAME=Example
+export GIT_AUTHOR_EMAIL=postmaster@example.org
+
+export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
 setup_repo() {
     git init .
-    git $AUTHOR_CONFIG commit --allow-empty -m 'First commit' "$AUTHOR_OPTION"
+    git commit --allow-empty -m 'First commit'
     git checkout -b "$MERGE_QUEUE"
     git checkout "$MASTER"
 
@@ -26,7 +29,7 @@ send_to_merge_queue() {
     git checkout "$MERGE_QUEUE"
     git merge --squash "$2"
     printf "%s\n\n%s" "$1" "$(cat .git/SQUASH_MSG)" > .git/SQUASH_MSG
-    git $AUTHOR_CONFIG commit -F .git/SQUASH_MSG "$AUTHOR_OPTION"
+    git commit -F .git/SQUASH_MSG
 }
 
 commit_merge_queue() {
